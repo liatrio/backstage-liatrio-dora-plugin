b51b5120ed1df840160e8b7bccfd4f24a4750402
Eliot Eikenberry <wolftousen@Tundria.local>
2024-07-16 09:55:14 -0700

    fix: force dynamic css since BS doesn't like loading our css

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,22 +1,36 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react'
 import {
   InfoCard,
-} from '@backstage/core-components';
-import { Box, Grid } from '@material-ui/core';
+} from '@backstage/core-components'
+import { Box, Grid } from '@material-ui/core'
 
-import { RecoverTime, ChangeFailureRate, ChangeLeadTime, DeploymentFrequency, ScoreBoard, fetchData } from 'liatrio-react-dora';
-import { useEntity } from '@backstage/plugin-catalog-react';
-import { useApi, configApiRef } from '@backstage/core-plugin-api';
-import { genAuthHeaderValueLookup, getRepoName } from '../helper';
-import DateRangePicker from 'rsuite/DateRangePicker';
-import { DateRange } from 'rsuite/DateRangePicker';
-import { startOfDay, endOfDay, addDays } from 'date-fns';
+import { RecoverTime, ChangeFailureRate, ChangeLeadTime, DeploymentFrequency, ScoreBoard, fetchData } from 'liatrio-react-dora'
+import { useEntity } from '@backstage/plugin-catalog-react'
+import { useApi, configApiRef } from '@backstage/core-plugin-api'
+import { genAuthHeaderValueLookup, getRepoName } from '../helper'
+
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+
+const addDynamicStyles = (className: string, styles: string) => {
+  const styleElement = document.createElement('style');
+  styleElement.innerHTML = `.${className} { ${styles} }`;
+  document.head.appendChild(styleElement);
+};
+
+const getDate = (daysInPast: number) : Date => {
+  let date = new Date()
+
+  date.setDate(date.getDate() - daysInPast)
+
+  return date
+}
 
 export const Charts = () => {
-  const entity = useEntity();
-  const configApi = useApi(configApiRef);
-  const backendUrl = configApi.getString('backend.baseUrl');
-  const endpoint = configApi.getString("dora.endpoint");
+  const entity = useEntity()
+  const configApi = useApi(configApiRef)
+  const backendUrl = configApi.getString('backend.baseUrl')
+  const endpoint = configApi.getString("dora.endpoint")
 
   const getAuthHeaderValue = genAuthHeaderValueLookup()
   
@@ -24,82 +38,104 @@ export const Charts = () => {
 
   const [repoName, setRepoName] = useState<string>("")
   const [data, setData] = useState<any>()
-  const startDate = useRef<Date>(new Date((new Date()).getTime() - 30 * 24 * 60 * 60 * 1000))
-  const endDate = useRef<Date>(new Date())
+  const [startDate, setStartDate] = useState<Date>(getDate(31))
+  const endDate = getDate(1)
+  const [selectedRange, setSelectedRange] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(true)
 
-  const ranges: any = [
-    {
-      label: '30 days',
-      value: [startOfDay(addDays(new Date(), -31)), endOfDay(addDays(new Date(), -1))]
-    },
-    {
-      label: '60 days',
-      value: [startOfDay(addDays(new Date(), -61)), endOfDay(addDays(new Date(), -1))]
-    },
-    {
-      label: '90 days',
-      value: [startOfDay(addDays(new Date(), -91)), endOfDay(addDays(new Date(), -1))]
-    },
-    {
-      label: '180 days',
-      value: [startOfDay(addDays(new Date(), -181)), endOfDay(addDays(new Date(), -1))]
-    },
-    {
-      label: '365 days',
-      value: [startOfDay(addDays(new Date(), -366)), endOfDay(addDays(new Date(), -1))]
+  const ranges: any = [{
+    value: 30, label: "Last 30 Days"
+  },{
+    value: 60, label: "Last 60 Days"
+  },{
+    value: 90, label: "Last 90 Days"
+  },{
+    value: 180, label: "Last 180 Days"
+  },{
+    value: 365, label: "Last 365 Days"
+  }]
+
+  const updateDateRange = async ( value: any ) => {
+    let newRange = ranges.findIndex((range: { value: number; label: string }) => range.value === value.value)
+
+    if(newRange === selectedRange) {
+      return
+    } else {
+      setSelectedRange(newRange)
     }
-  ]
 
-  const updateDateRange = async ( value: DateRange ) => {
-    const start = value[0]
-    const end = value[1]
+    const newStartDate = getDate(value.value)
 
-    if(start.getTime() !== startDate.current.getTime() || end.getTime() !== endDate.current.getTime()) {
-      await fetchData({
+    setStartDate(newStartDate)
+    setLoading(true)
+    await fetchData({
         api: apiUrl,
         getAuthHeaderValue: getAuthHeaderValue,
         repositories: [repoName],
-        start: start,
-        end: end,
+        start: newStartDate,
+        end: endDate,
       }, (data: any) => {
         setData(data)
+        setLoading(false)
+      }, (_) => {
+        setLoading(false)
       })
-    }
-    
-    startDate.current = start
-    endDate.current = end
   }
 
   useEffect(() => {
     const repoName = getRepoName(entity)
     setRepoName(repoName)
-
+    setLoading(true)
     let fetch = async () => {
       fetchData({
         api: apiUrl,
         getAuthHeaderValue: getAuthHeaderValue,
         repositories: [repoName],
-        start: startDate.current,
-        end: endDate.current,
+        start: startDate,
+        end: endDate,
       }, (data: any) => {
         setData(data)
+        setLoading(false)
+      }, (_) => {
+        setLoading(false)
       })
     }
 
     fetch()
   }, [])
 
+  useEffect(() => {
+    // Define your styles as a string
+    const styles = `
+      overflow: visible;
+    `;
+
+    // Add the styles to the document
+    addDynamicStyles('doraOptions', styles);
+  }, []);
+
   if(repoName === "") {
     return (<div>DORA Metrics are not available for Non-GitHub repos currently</div>)
   }
 
   return (<>
-    <Grid container spacing={3} alignItems="stretch">
-      <Grid item md={6} style={{paddingBottom: "25px"}}>
-        <InfoCard title="Options">
+    <Grid container style={{marginBottom: "10px"}} spacing={3} alignItems="stretch">
+      <Grid item md={6} style={{paddingBottom: "25px", overflow: "visible"}}>
+        <InfoCard title="Options" className="doraOptions">
+          <Box overflow="visible" position="relative">
+            <Box overflow="visible" display="flex" justifyContent="center" alignItems="center">
+              <label style={{paddingRight: "10px"}}>Select Days in past to view:</label><Dropdown options={ranges} onChange={updateDateRange} value={ranges[selectedRange]} />
+            </Box>
+          </Box>
+        </InfoCard>
+      </Grid>
+      <Grid item md={6}>
+        <InfoCard title="DORA: At a Glance">
           <Box position="relative">
             <Box display="flex" justifyContent="flex-end">
-              <DateRangePicker onOk={updateDateRange} ranges={ranges} value={ranges[0].value}/>
+              <div style={{ width: '100%', height: '100px' }}>
+                <ScoreBoard data={data} loading={loading}/>
+              </div>
             </Box>
           </Box>
         </InfoCard>
@@ -111,7 +147,7 @@ export const Charts = () => {
           <Box position="relative">
             <Box display="flex" justifyContent="flex-end">
               <div style={{ width: '800px', height: '400px' }}>
-                <DeploymentFrequency data={data}/>
+                <DeploymentFrequency data={data} loading={loading}/>
               </div>
             </Box>
           </Box>
@@ -122,7 +158,7 @@ export const Charts = () => {
           <Box position="relative">
             <Box display="flex" justifyContent="flex-end">
               <div style={{ width: '800px', height: '400px' }}>
-                <ChangeLeadTime data={data}/>
+                <ChangeLeadTime data={data} loading={loading}/>
               </div>
             </Box>
           </Box>
@@ -133,7 +169,7 @@ export const Charts = () => {
           <Box position="relative">
             <Box display="flex" justifyContent="flex-end">
               <div style={{ width: '800px', height: '400px' }}>
-                <ChangeFailureRate data={data}/>
+                <ChangeFailureRate data={data} loading={loading}/>
               </div>
             </Box>
           </Box>
@@ -144,7 +180,7 @@ export const Charts = () => {
           <Box position="relative">
             <Box display="flex" justifyContent="flex-end">
               <div style={{ width: '800px', height: '400px' }}>
-                <RecoverTime data={data}/>
+                <RecoverTime data={data} loading={loading}/>
               </div>
             </Box>
           </Box>
